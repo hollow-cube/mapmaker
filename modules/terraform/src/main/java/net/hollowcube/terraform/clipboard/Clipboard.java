03226ea96ab7b087d0b811afebd2f9be637d9b60
mworzala <mattheworzala@gmail.com>
2022-11-30 17:52:43 -0500

    initial clipboard test

---
+++ modules/terraform/src/main/java/net/hollowcube/terraform/clipboard/Clipboard.java
@@ -0,0 +1,18 @@
+package net.hollowcube.terraform.clipboard;
+
+import net.hollowcube.util.schem.Schematic;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Clipboard {
+    private Schematic current = null;
+
+    public void set(@NotNull Schematic schematic) {
+        this.current = schematic;
+    }
+
+    public @Nullable Schematic get() {
+        return current;
+    }
+
+}
