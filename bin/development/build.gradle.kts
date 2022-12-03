d70a597d7b8b016e9de72238c12f3378517879c2
mworzala <mattheworzala@gmail.com>
2022-12-03 15:32:23 -0500

    Map saving/loading using instance module

--- bin/development/build.gradle.kts
+++ bin/development/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
 
     val commonVersion = rootProject.property("commonVersion")
     implementation("com.github.hollow-cube.common:common:${commonVersion}")
-    implementation("com.github.hollow-cube.common:block-placement:${commonVersion}")
 
     implementation(project(":modules:common"))
     implementation(project(":modules:hub"))
