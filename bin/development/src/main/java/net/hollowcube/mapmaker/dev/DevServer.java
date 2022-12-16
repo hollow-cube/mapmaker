803728b894ae245c660a0208b0d38113c75e29fa
mworzala <mattheworzala@gmail.com>
2022-12-15 23:31:37 -0500

    dev server helm chart, add separated build action

--- bin/development/src/main/java/net/hollowcube/mapmaker/dev/DevServer.java
+++ bin/development/src/main/java/net/hollowcube/mapmaker/dev/DevServer.java
@@ -1,5 +1,8 @@
 package net.hollowcube.mapmaker.dev;
 
+import io.helidon.health.HealthSupport;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
 import net.hollowcube.map.MapServer;
 import net.hollowcube.map.world.MapWorld;
 import net.hollowcube.mapmaker.hub.HubServer;
@@ -11,24 +14,19 @@ import net.hollowcube.mapmaker.storage.PlayerStorage;
 import net.hollowcube.mapmaker.storage.Storage;
 import net.hollowcube.mapmaker.util.StaticAbuse;
 import net.kyori.adventure.bossbar.BossBar;
-import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.translation.GlobalTranslator;
-import net.kyori.adventure.translation.TranslationRegistry;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.adventure.MinestomAdventure;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.event.player.AsyncPlayerPreLoginEvent;
 import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.extras.MojangAuth;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.jetbrains.annotations.NotNull;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.PropertyResourceBundle;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class DevServer {
@@ -36,22 +34,43 @@ public class DevServer {
         System.setProperty("minestom.terminal.disabled", "true");
         System.setProperty("hc.instance.temp_dir", "./bin/development/build/local/local-maps");
 
-        var server = MinecraftServer.init();
+        var minecraftServer = MinecraftServer.init();
+        var server = new DevServer();
 
-        MojangAuth.init();
+        WebServer webServer = WebServer.builder()
+                .port(9124)
+                .addRouting(Routing.builder()
+                        .register(HealthSupport.builder()
+                                .webContext("alive")
+                                .addLiveness()
+                                .build())
+                        .register(HealthSupport.builder()
+                                .webContext("ready")
+                                .addReadiness()
+                                .build())
+                        .build())
+                .build();
+        webServer.start()
+                .thenAccept(ws -> System.out.println("Web server is running at :" + ws.port()));
+        //todo handle kill code and stop everything gracefully.
 
-        new DevServer();
-
-        server.start("0.0.0.0", 25565);
+        server.start();
+        minecraftServer.start("0.0.0.0", 25565);
     }
 
-    private final PlayerStorage playerStorage;
-    private final MapStorage mapStorage;
+    private PlayerStorage playerStorage;
+    private MapStorage mapStorage;
 
-    private final HubServer hub;
-    private final MapServer maps;
+    private HubServer hub;
+    private MapServer maps;
 
     public DevServer() {
+
+    }
+
+    public void start() {
+        MojangAuth.init();
+
         var mongoUri = System.getenv("MM_MONGO_URI");
         if (mongoUri == null) {
             this.playerStorage = PlayerStorage.memory();
@@ -72,6 +91,13 @@ public class DevServer {
         eventHandler.addListener(PlayerSpawnEvent.class, this::handleFirstSpawn);
 
         registerCommands();
+    }
+
+    public @NotNull List<HealthCheck> readinessChecks() {
+        return List.of(
+                () -> MinecraftServer.isStarted() ? HealthCheckResponse.up("minestom") : HealthCheckResponse.down("minestom"),
+                () -> HealthCheckResponse.up("mapmaker")
+        );
     }
 
     private void registerCommands() {
--- modules/common/build.gradle.kts
+++ modules/common/build.gradle.kts
@@ -1,4 +1,11 @@
+plugins {
+    `java-library`
+}
 
 dependencies {
     implementation("org.mongodb:mongodb-driver-sync:4.7.0")
+
+    // Helidon
+    api(platform("io.helidon:helidon-dependencies:3.0.2"))
+    api("io.helidon.health:helidon-health")
 }
