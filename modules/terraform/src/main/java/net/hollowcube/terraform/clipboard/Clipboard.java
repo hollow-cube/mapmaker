4ad5c2884537294ab714b1333517ee9409acf70f
GreatWyrm <alecmusante@gmail.com>
2022-12-16 17:53:22 -0800

    Added rotations to clipboard

--- modules/terraform/src/main/java/net/hollowcube/terraform/clipboard/Clipboard.java
+++ modules/terraform/src/main/java/net/hollowcube/terraform/clipboard/Clipboard.java
@@ -1,11 +1,13 @@
 package net.hollowcube.terraform.clipboard;
 
+import net.hollowcube.util.schem.Rotation;
 import net.hollowcube.util.schem.Schematic;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Clipboard {
     private Schematic current = null;
+    private Rotation currentRotation = Rotation.NONE;
 
     public void set(@NotNull Schematic schematic) {
         this.current = schematic;
@@ -13,5 +15,13 @@ public class Clipboard {
 
     public @Nullable Schematic get() {
         return current;
+    }
+
+    public Rotation getCurrentRotation() {
+        return currentRotation;
+    }
+
+    public void setCurrentRotation(Rotation currentRotation) {
+        this.currentRotation = currentRotation;
     }
 }
--- modules/terraform/src/main/java/net/hollowcube/terraform/compat/worldedit/command/ClipboardCommands.java
+++ modules/terraform/src/main/java/net/hollowcube/terraform/compat/worldedit/command/ClipboardCommands.java
@@ -6,15 +6,18 @@ import net.hollowcube.util.schem.Rotation;
 import net.hollowcube.util.schem.SchematicBuilder;
 import net.minestom.server.command.CommandManager;
 import net.minestom.server.command.CommandSender;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.arguments.ArgumentWord;
 import net.minestom.server.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class ClipboardCommands {
     public ClipboardCommands(@NotNull CommandManager commands) {
-
         commands.register(CommandUtil.singleSyntaxCommand("/copy", this::copy));
         commands.register(CommandUtil.singleSyntaxCommand("/paste", this::paste));
-
+        commands.register(new RotateCommand());
     }
 
     public void copy(@NotNull CommandSender sender) {
@@ -43,15 +46,45 @@ public class ClipboardCommands {
         if (!(sender instanceof Player player))
             throw new UnsupportedOperationException("only implemented for players");
 
-        var clipboard = Session.forPlayer(player).getClipboard().get();
-        if (clipboard == null) {
+        var clipboard = Session.forPlayer(player).getClipboard();
+        var schem = clipboard.get();
+        if (schem == null) {
             player.sendMessage("No clipboard contents");
             return;
         }
 
         //todo need to add to history. Should make a generic api for applying
-        clipboard.build(Rotation.NONE, null).apply(player.getInstance(), player.getPosition(), null);
+        schem.build(clipboard.getCurrentRotation(), null).apply(player.getInstance(), player.getPosition(), null);
         sender.sendMessage("Pasted clipboard contents");
     }
 
+    public static class RotateCommand extends Command {
+        private final ArgumentWord rotateArg = ArgumentType.Word("rotation").from("90", "180", "270");
+
+        public RotateCommand() {
+            super("/rotate");
+
+            setDefaultExecutor((sender, context) -> sender.sendMessage("Usage: //rotate <degrees>"));
+            addSyntax(this::rotateClipboard, rotateArg);
+        }
+        public void rotateClipboard(@NotNull CommandSender sender, @NotNull CommandContext context) {
+            if (!(sender instanceof Player player))
+                throw new UnsupportedOperationException("only implemented for players");
+
+            var clipboard = Session.forPlayer(player).getClipboard();
+
+            if (clipboard.get() == null) {
+                player.sendMessage("Nothing in your clipboard to rotate.");
+                return;
+            }
+
+            var degrees = context.get(rotateArg);
+            switch (degrees) {
+                case "90" -> clipboard.setCurrentRotation(Rotation.CLOCKWISE_90);
+                case "180" -> clipboard.setCurrentRotation(Rotation.CLOCKWISE_180);
+                case "270" -> clipboard.setCurrentRotation(Rotation.CLOCKWISE_270);
+                default -> player.sendMessage("Unknown rotation value " + degrees);
+            }
+        }
+    }
 }
