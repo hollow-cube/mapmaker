e6203d64914ed4a506f63c4bb23a97e128bc31f8
mworzala <mattheworzala@gmail.com>
2022-12-18 21:50:07 -0500

    add instructions for running dev server

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     id("net.ltgt.errorprone") version "2.0.2" apply false
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
 }
+
+allprojects {
+    tasks.withType<Copy> {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+}
