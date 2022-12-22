a067f90b5b90c8ec57482594298a6caeb4f8b533
mworzala <mattheworzala@gmail.com>
2022-12-22 13:27:03 -0500

    initial chat port (not working i am sure)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,13 @@ rootProject.name = "mapmaker"
 include(":modules")
 // Standalone modules
 include(":modules:canvas")
+include(":modules:terraform")
+include(":modules:chat")
 
 // Mapmaker modules
 include(":modules:common")
 include(":modules:hub")
 include(":modules:map")
-include(":modules:terraform")
 
 // Binaries
 include(":bin")
