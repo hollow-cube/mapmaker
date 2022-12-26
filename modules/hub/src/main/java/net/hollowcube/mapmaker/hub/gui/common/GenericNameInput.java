602bab483348fe05875bdc5b4724b8a96d6eb1fd
mworzala <mattheworzala@gmail.com>
2022-12-26 07:54:36 -0500

    Make map slot view dynamic

--- modules/hub/src/main/java/net/hollowcube/mapmaker/hub/gui/common/GenericNameInput.java
+++ modules/hub/src/main/java/net/hollowcube/mapmaker/hub/gui/common/GenericNameInput.java
@@ -13,30 +13,32 @@ import java.util.function.Consumer;
 
 //todo this is very basic and wont be valid forever, eg for font stuff, checking if a name is valid, etc.
 public class GenericNameInput extends AnvilSection {
-    private static final ItemStack INPUT_SLOT = ItemStack.of(Material.PAPER)
-            .withDisplayName(Component.text("Untitled").decoration(TextDecoration.ITALIC, false));
 
     private final Consumer<String> callback;
 
     /**
      * Creates a name input with the given callback.
      * <p>
-     * The callback is executed after the player clicks the confirm button, <i>before</i> the history has been popped
-     * (eg the current view will still be the name input).
+     * The callback is executed after the player clicks the confirm button, <i>after</i> the history has been popped
+     * (eg the current view will be whatever the one was before the anvil).
      */
-    public GenericNameInput(@NotNull Consumer<String> callback) {
+    public GenericNameInput(@NotNull String initial, @NotNull Consumer<String> callback) {
         this.callback = callback;
 
-        add(0, 0, new ButtonSection(1, 1, INPUT_SLOT, () -> {}));
+        var inputItem = ItemStack.of(Material.PAPER)
+                .withDisplayName(Component.text(initial).decoration(TextDecoration.ITALIC, false));
+
+        add(0, 0, new ButtonSection(1, 1, inputItem, () -> {}));
         add(2, 0, new ButtonSection(1, 1, ItemStack.AIR, this::handleConfirm));
     }
 
     private void handleConfirm() {
-        callback.accept(getInput());
 
         // Return to last view
         var router = find(RouterSection.class);
         router.pop();
+
+        callback.accept(getInput());
     }
 
 }
