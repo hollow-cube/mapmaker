b121ace21b39f436b28e7a4a2697fb63271c7999
mworzala <mattheworzala@gmail.com>
2022-12-27 21:23:50 -0500

    add prometheus exporter & some initial metrics for timing handler calls

---
+++ modules/map/src/main/java/net/hollowcube/map/option/TimeOfDayOption.java
@@ -0,0 +1,12 @@
+package net.hollowcube.map.option;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TimeOfDayOption implements MapOption {
+
+    @Override
+    public @NotNull String id() {
+        return "time_of_day";
+    }
+
+}
