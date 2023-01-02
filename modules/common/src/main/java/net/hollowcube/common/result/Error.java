17b1a7208b12b79f7ab27451f7e1bcaf1e542a5e
mworzala <mattheworzala@gmail.com>
2023-01-02 15:59:25 -0500

    fix error reporting bug

--- modules/common/src/main/java/net/hollowcube/common/result/Error.java
+++ modules/common/src/main/java/net/hollowcube/common/result/Error.java
@@ -2,8 +2,6 @@ package net.hollowcube.common.result;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.text.MessageFormat;
-
 @SuppressWarnings("JavaLangClash") //todo probably should rename to Err or something
 public sealed interface Error permits Errors.ExceptionError, Errors.MultiError, Errors.SimpleError, Errors.WrappedError {
 
@@ -18,7 +16,7 @@ public sealed interface Error permits Errors.ExceptionError, Errors.MultiError, 
     @NotNull String message();
 
     default @NotNull Error wrap(@NotNull String message) {
-        return new Errors.WrappedError(MessageFormat.format(message, this.message()), this);
+        return new Errors.WrappedError(message + this.message(), this);
     }
 
     default boolean is(@NotNull Error error) {
