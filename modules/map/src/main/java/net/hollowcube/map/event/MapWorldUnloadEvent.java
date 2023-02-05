f2d17f0ee03249b18f2b18dcc311c4cce9f1868d
mworzala <mattheworzala@gmail.com>
2023-02-04 21:27:15 -0500

    initial experimentation with marker tool

---
+++ modules/map/src/main/java/net/hollowcube/map/event/MapWorldUnloadEvent.java
@@ -0,0 +1,20 @@
+package net.hollowcube.map.event;
+
+import net.hollowcube.map.event.trait.MapWorldEvent;
+import net.hollowcube.map.world.MapWorld;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Called when a world loads in a map. ONLY called if there is a saved world.
+ * @param mapWorld
+ */
+public record MapWorldUnloadEvent(
+        @NotNull MapWorld mapWorld
+) implements MapWorldEvent {
+
+    @Override
+    public @NotNull MapWorld getMapWorld() {
+        return mapWorld;
+    }
+
+}
