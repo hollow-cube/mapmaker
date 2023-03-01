fe8ba3e37f983298f18d187fef12ad482b9c3248
GreatWyrm <alecmusante@gmail.com>
2023-02-28 21:44:55 -0800

    Resolve some todos

---
+++ modules/terraform/src/main/java/net/hollowcube/terraform/compat/worldedit/command/GeneralCommands.java
@@ -0,0 +1,50 @@
+package net.hollowcube.terraform.compat.worldedit.command;
+
+import net.hollowcube.terraform.session.LocalSession;
+import net.minestom.server.command.CommandSender;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public final class GeneralCommands {
+    private GeneralCommands() {}
+
+    public static final class Undo extends Command {
+
+        public Undo() {
+            super("/undo");
+
+            setDefaultExecutor(this::undoblah);
+        }
+
+        private void undoblah(@NotNull CommandSender sender, @NotNull CommandContext context) {
+            if (!(sender instanceof Player player))
+                throw new UnsupportedOperationException("only implemented for players");
+
+            var session = LocalSession.forPlayer(player);
+            int undone = session.undo(1);
+            player.sendMessage("Undone " + undone + " actions");
+        }
+
+    }
+
+    public static final class Redo extends Command {
+
+        public Redo() {
+            super("/redo");
+
+            setDefaultExecutor(this::redoblah);
+        }
+
+        private void redoblah(@NotNull CommandSender sender, @NotNull CommandContext context) {
+            if (!(sender instanceof Player player))
+                throw new UnsupportedOperationException("only implemented for players");
+
+            var session = LocalSession.forPlayer(player);
+            int undone = session.redo(1);
+            player.sendMessage("Redone " + undone + " actions");
+        }
+
+    }
+}
