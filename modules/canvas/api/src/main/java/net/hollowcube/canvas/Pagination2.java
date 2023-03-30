ae692882afbe9fec93b95c01f03f22edc6f0e8e0
mworzala <mattheworzala@gmail.com>
2023-03-29 21:27:27 -0400

    start pagination stuff

---
+++ modules/canvas/api/src/main/java/net/hollowcube/canvas/Pagination2.java
@@ -0,0 +1,28 @@
+package net.hollowcube.canvas;
+
+import net.hollowcube.canvas.internal.Context;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public interface Pagination2 extends Element {
+
+    void nextPage();
+
+    void prevPage();
+
+    interface PageRequest<T extends View> {
+        @NotNull Context context();
+
+        int page();
+        int pageSize();
+
+        /**
+         * Called to supply a response to the page request.
+         * <p>
+         * Implementations must be thread-safe.
+         */
+        void respond(@NotNull List<@NotNull T> view, boolean nextPage);
+    }
+
+}
