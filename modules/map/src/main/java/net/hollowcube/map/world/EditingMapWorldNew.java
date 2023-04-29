b0966ce402e5fa7e64cc5e5d9443f43a26bb3d11
mworzala <mattheworzala@gmail.com>
2023-04-29 09:08:10 +0300

    semi working test world, issue with entities in instances tho

--- modules/map/src/main/java/net/hollowcube/map/world/EditingMapWorldNew.java
+++ modules/map/src/main/java/net/hollowcube/map/world/EditingMapWorldNew.java
@@ -1,25 +1,18 @@
 package net.hollowcube.map.world;
 
-import jdk.incubator.concurrent.StructuredTaskScope;
-import net.hollowcube.map.MapHooks;
 import net.hollowcube.map.MapServer;
-import net.hollowcube.map.event.MapWorldPlayerStartPlayingEvent;
-import net.hollowcube.map.event.MapWorldPlayerStopPlayingEvent;
 import net.hollowcube.map.feature.FeatureProvider;
 import net.hollowcube.map.item.ItemRegistry;
 import net.hollowcube.map.util.StringUtil;
 import net.hollowcube.mapmaker.model.MapData;
 import net.hollowcube.mapmaker.model.PlayerData;
 import net.hollowcube.mapmaker.model.SaveState;
-import net.hollowcube.mapmaker.storage.SaveStateStorage;
 import net.hollowcube.world.BaseWorld;
 import net.hollowcube.world.dimension.DimensionTypes;
 import net.hollowcube.world.generation.MapGenerators;
 import net.minestom.server.coordinate.Point;
-import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
-import net.minestom.server.event.EventDispatcher;
 import net.minestom.server.event.EventFilter;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.trait.InstanceEvent;
@@ -30,12 +23,10 @@ import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.Blocking;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.Future;
 
+@SuppressWarnings("UnstableApiUsage")
 public class EditingMapWorldNew implements InternalMapWorldNew {
     private static final System.Logger logger = System.getLogger(EditingMapWorldNew.class.getName());
 
@@ -51,7 +42,7 @@ public class EditingMapWorldNew implements InternalMapWorldNew {
 
     private final List<FeatureProvider> enabledFeatures = new ArrayList<>();
     private final ItemRegistry itemRegistry;
-    private final EventNode<InstanceEvent> eventNode = EventNode.event("world-local", EventFilter.INSTANCE, ev -> {
+    private final EventNode<InstanceEvent> scopedNode = EventNode.event("world-local", EventFilter.INSTANCE, ev -> {
         if (ev instanceof PlayerEvent event) {
             return event.getPlayer().hasTag(TAG_EDITING);
         }
@@ -67,8 +58,11 @@ public class EditingMapWorldNew implements InternalMapWorldNew {
         this.baseWorld = new BaseWorld(server.worldManager(), map.getId(), instance);
         instance.setGenerator(MapGenerators.voidWorld());
         instance.setTag(SELF_TAG, this);
+        var eventNode = instance.eventNode();
 
         this.itemRegistry = new ItemRegistry();
+        eventNode.addChild(itemRegistry.eventNode());
+        eventNode.addChild(scopedNode);
     }
 
     @Override
@@ -93,12 +87,12 @@ public class EditingMapWorldNew implements InternalMapWorldNew {
 
     @Override
     public void addScopedEventNode(@NotNull EventNode<InstanceEvent> eventNode) {
-        eventNode.addChild(eventNode);
+        this.scopedNode.addChild(eventNode);
     }
 
     @Override
     public @NotNull Point spawnPoint() {
-        return new Vec(0.5, 40, 0.5);
+        return map.getSpawnPoint();
     }
 
     @Override
@@ -113,28 +107,7 @@ public class EditingMapWorldNew implements InternalMapWorldNew {
             baseWorld.loadWorld();
         }
 
-        // Load each of the features
-        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            // Load each feature in parallel
-            var features = server.features();
-            var enabledFutures = new Future[features.size()];
-            for (int i = 0; i < features.size(); i++) {
-                var feature = features.get(i);
-                enabledFutures[i] = scope.fork(() -> feature.initMap(this));
-            }
-
-            scope.join();
-
-            // Add each feature to the enabled list if it is enabled.
-            for (int i = 0; i < features.size(); i++) {
-                var feature = features.get(i);
-                if ((boolean) enabledFutures[i].resultNow()) {
-                    enabledFeatures.add(feature);
-                }
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        this.enabledFeatures.addAll(MapWorldHelpers.loadFeatures(this));
     }
 
     @Override
@@ -158,41 +131,21 @@ public class EditingMapWorldNew implements InternalMapWorldNew {
     public @Blocking void acceptPlayer(@NotNull Player player) {
         var playerData = PlayerData.fromPlayer(player);
 
+        var saveState = MapWorldHelpers.getOrCreateSaveState(this, playerData.getId(), SaveState.Type.EDITING);
+
         player.setTag(TAG_EDITING, true);
-        player.setGameMode(GameMode.CREATIVE); //todo based on setting
+        player.setTag(SaveState.TAG, saveState);
         player.refreshCommands();
 
-        var saveStateStorage = server.saveStateStorage();
-        SaveState saveState;
-        try {
-            saveState = saveStateStorage.getLatestSaveState(playerData.getId(), map().getId(), SaveState.Type.EDITING);
-        } catch (SaveStateStorage.NotFoundError e) {
-            // common savestate creation logic todo move elsewhere
-            saveState = new SaveState();
-            saveState.setId(UUID.randomUUID().toString());
-            saveState.setPlayerId(playerData.getId());
-            saveState.setMapId(map().getId());
-            saveState.setStartTime(Instant.now());
-            saveState.setPos(map.getSpawnPoint());
-
-            saveState.setType(SaveState.Type.EDITING);
-
-            saveStateStorage.createSaveState(saveState);
-
-            player.setTag(MapHooks.PLAYING, true);
-            player.setTag(SaveState.TAG, saveState);
-        }
-
-        // formerly initPlayerFromSaveState
         var inventory = saveState.getInventory();
+        player.getInventory().clear();
         for (int i = 0; i < inventory.size(); i++) {
             player.getInventory().setItemStack(i, inventory.get(i));
         }
         player.setGameMode(GameMode.CREATIVE);
         player.teleport(saveState.getPos()).join();
-        player.sendMessage("Now editing " + map.getName());
 
-        EventDispatcher.call(new MapWorldPlayerStartPlayingEvent(this, player));
+        player.sendMessage("Now editing " + map.getName());
     }
 
     @Override
@@ -211,8 +164,6 @@ public class EditingMapWorldNew implements InternalMapWorldNew {
             } catch (Exception e) {
                 logger.log(System.Logger.Level.ERROR, "Failed to save player state for {0}", player.getUuid(), e);
             }
-
-            EventDispatcher.call(new MapWorldPlayerStopPlayingEvent(this, player));
         }
 
         player.removeTag(TAG_EDITING);
