102dd1803242ecbf66807d99a7e1381a428d3569
mworzala <mattheworzala@gmail.com>
2023-06-11 16:56:41 -0400

    implement some right clicking states

--- modules/map/src/main/java/net/hollowcube/map/feature/edit/SpecialClickHandlingFeatureProvider.java
+++ modules/map/src/main/java/net/hollowcube/map/feature/edit/SpecialClickHandlingFeatureProvider.java
@@ -1,25 +1,28 @@
 package net.hollowcube.map.feature.edit;
 
 import com.google.auto.service.AutoService;
+import it.unimi.dsi.fastutil.ints.Int2IntArrayMap;
 import net.hollowcube.map.feature.FeatureProvider;
 import net.hollowcube.map.world.MapWorld;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventFilter;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.player.PlayerBlockInteractEvent;
+import net.minestom.server.event.player.PlayerUseItemOnBlockEvent;
 import net.minestom.server.event.trait.InstanceEvent;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.item.Material;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Set;
 
 @AutoService(FeatureProvider.class)
 public class SpecialClickHandlingFeatureProvider implements FeatureProvider {
 
     private final EventNode<InstanceEvent> eventNode = EventNode.type("special-click-handler", EventFilter.INSTANCE)
-            .addListener(PlayerBlockInteractEvent.class, this::handeBlockInteract);
+            .addListener(PlayerUseItemOnBlockEvent.class, this::handleUseItem)
+            .addListener(PlayerBlockInteractEvent.class, this::handleShiftClick);
 
-    private static final Set<String> TOGGLEABLE_STATES = Set.of("lit", "extendable");
+    private static final Int2IntArrayMap STRIP_MAP;
+    private static final Int2IntArrayMap CANDLE_CAKE_MAP;
 
     @Override
     public boolean initMap(@NotNull MapWorld world) {
@@ -29,41 +32,66 @@ public class SpecialClickHandlingFeatureProvider implements FeatureProvider {
         return true;
     }
 
-    private void handeBlockInteract(PlayerBlockInteractEvent event) {
+    private void handleUseItem(PlayerUseItemOnBlockEvent event) {
+        if (event.getHand() != Player.Hand.MAIN) return;
+
+        var itemStack = event.getItemStack();
+        var block = event.getInstance().getBlock(event.getPosition());
+
+        String state;
+        int strippedState;
+        if ("false".equals(block.getProperty("waterlogged")) && itemStack.material().equals(Material.WATER_BUCKET)) {
+            block = block.withProperty("waterlogged", "true");
+        } else if ("true".equals(block.getProperty("waterlogged")) && itemStack.material().equals(Material.BUCKET)) {
+            block = block.withProperty("waterlogged", "false");
+        } else if ((strippedState = STRIP_MAP.get(block.id())) != 0 && itemStack.material().name().contains("axe")) {
+            block = Block.fromBlockId(strippedState).withProperties(block.properties());
+        } else return;
+
+        //todo the block update isnt being sent correctly here, this is a minestom bug
+        event.getInstance().setBlock(event.getPosition(), block);
+    }
+
+    private void handleShiftClick(PlayerBlockInteractEvent event) {
         if (event.getHand() != Player.Hand.MAIN || !event.getPlayer().isSneaking()) return;
 
+        //todo waterlogging shouldnt be on shift click, handle with a different eventr
+//        var item = event.getPlayer().getItemInHand(event.getHand());
+
+
         var block = event.getBlock();
-        boolean changed = false;
 
         String state;
         if ((state = block.getProperty("lit")) != null) {
             block = block.withProperty("lit", state.equals("true") ? "false" : "true");
-            changed = true;
         } else if ((state = block.getProperty("extendable")) != null) {
             block = block.withProperty("extendable", state.equals("true") ? "false" : "true");
-            changed = true;
         } else if (block.id() == Block.FARMLAND.id()) {
             state = block.getProperty("moisture");
             block = block.withProperty("moisture", state.equals("7") ? "0" : "7");
-            changed = true;
         } else if (block.name().contains("trapdoor") || block.id() == Block.BARREL.id()) {
             // todo should use trapdoor block tag i guess
             state = block.getProperty("open");
             block = block.withProperty("open", state.equals("true") ? "false" : "true");
-            changed = true;
         } else if (block.id() == Block.CAKE.id()) {
             var bites = Integer.parseInt(block.getProperty("bites"));
             block = block.withProperty("bites", String.valueOf((bites + 1) % 7));
-            changed = true;
         } else if (block.id() == Block.COMPOSTER.id()) {
             var level = Integer.parseInt(block.getProperty("level"));
             block = block.withProperty("level", String.valueOf((level + 1) % 9));
-            changed = true;
-        }
+        } else return; // If we hit this then exit, otherwise we will update the block
 
-        if (changed) {
-            // Update the block in the world to the new state
-            event.getInstance().setBlock(event.getBlockPosition(), block);
-        }
+        // Update the block in the world to the new state
+        event.getInstance().setBlock(event.getBlockPosition(), block);
+    }
+
+    static {
+        STRIP_MAP = new Int2IntArrayMap();
+        STRIP_MAP.put(Block.OAK_LOG.id(), Block.STRIPPED_OAK_LOG.id());
+        STRIP_MAP.put(Block.STRIPPED_OAK_LOG.id(), Block.OAK_LOG.id());
+
+        CANDLE_CAKE_MAP = new Int2IntArrayMap();
+        CANDLE_CAKE_MAP.put(Block.CANDLE.id(), Block.CANDLE_CAKE.id());
+        CANDLE_CAKE_MAP.put(Block.BLACK_CANDLE.id(), Block.BLACK_CANDLE_CAKE.id());
     }
 }
