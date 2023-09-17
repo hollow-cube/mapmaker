8ba8361586da7840898a6b0d9a15f446f78ece32
Nixotica <nixotica@gmail.com>
2023-09-17 16:32:20 -0700

    impl of new settings

---
+++ modules/map/src/main/java/net/hollowcube/map/feature/play/NoSprintFeatureProvider.java
@@ -0,0 +1,54 @@
+package net.hollowcube.map.feature.play;
+
+import com.google.auto.service.AutoService;
+import net.hollowcube.map.MapHooks;
+import net.hollowcube.map.event.MapPlayerInitEvent;
+import net.hollowcube.map.event.MapPlayerResetTriggerEvent;
+import net.hollowcube.map.event.MapWorldPlayerStopPlayingEvent;
+import net.hollowcube.map.feature.FeatureProvider;
+import net.hollowcube.map.world.MapWorld;
+import net.hollowcube.mapmaker.map.MapVariant;
+import net.minestom.server.coordinate.Point;
+import net.minestom.server.event.EventDispatcher;
+import net.minestom.server.event.EventFilter;
+import net.minestom.server.event.EventNode;
+import net.minestom.server.event.player.PlayerMoveEvent;
+import net.minestom.server.event.player.PlayerStartSprintingEvent;
+import net.minestom.server.event.player.PlayerStopSprintingEvent;
+import net.minestom.server.event.trait.InstanceEvent;
+import net.minestom.server.tag.Tag;
+import org.jetbrains.annotations.NotNull;
+
+@AutoService(FeatureProvider.class)
+public class NoSprintFeatureProvider implements FeatureProvider {
+    private final EventNode<InstanceEvent> eventNode = EventNode.type("mapmaker:play/nosprint", EventFilter.INSTANCE)
+            .addListener(MapPlayerInitEvent.class, this::initPlayer)
+            .addListener(MapWorldPlayerStopPlayingEvent.class, this::removePlayer);
+
+    @Override
+    public boolean initMap(@NotNull MapWorld world) {
+        if ((world.flags() & (MapWorld.FLAG_PLAYING|MapWorld.FLAG_TESTING)) == 0)
+            return false;
+
+        var settings = world.map().settings();
+        if (settings.getVariant() != MapVariant.PARKOUR || !settings.isNoSprint())
+            return false;
+
+        world.addScopedEventNode(eventNode);
+
+        return true;
+    }
+
+    public void initPlayer(@NotNull MapPlayerInitEvent event) {
+        var player = event.getPlayer();
+        if (!MapHooks.isPlayerPlaying(player)) return;
+
+        player.setFood(6);
+    }
+
+    public void removePlayer(@NotNull MapWorldPlayerStopPlayingEvent event) {
+        var player = event.getPlayer();
+
+        player.setFood(20);
+    }
+}
