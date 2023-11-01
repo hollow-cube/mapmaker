d6656b4dd16613ade66ff99b31c93ada9c0304a6
mworzala <mattheworzala@gmail.com>
2023-10-31 22:52:52 -0400

    rework runtime info system to work easier with bazel sandboxing

--- bin/development/src/main/java/net/hollowcube/mapmaker/dev/runtime/DevRuntime.java
+++ bin/development/src/main/java/net/hollowcube/mapmaker/dev/runtime/DevRuntime.java
@@ -3,31 +3,54 @@ package net.hollowcube.mapmaker.dev.runtime;
 import com.google.auto.service.AutoService;
 import net.hollowcube.common.ServerRuntime;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * File is intentionally NOT added to the Bazel project. It uses the template version.
- */
+import java.util.Properties;
+
 @AutoService(ServerRuntime.class)
 public class DevRuntime implements ServerRuntime {
+    private static final Logger logger = LoggerFactory.getLogger(DevRuntime.class);
+
+    private String version = "3.0.0";
+    private String commit = "dev";
+    private String minestom = "unknown";
+    private String resourcePackHash = "dev";
+
+    public DevRuntime() {
+        try (var is = getClass().getResourceAsStream("/runtime.properties")) {
+            if (is == null) return;
+
+            var props = new Properties();
+            props.load(is);
+
+            version = props.getOrDefault("version", "3.0.0").toString();
+            commit = props.getOrDefault("commit_sha", "dev").toString();
+            minestom = props.getOrDefault("minestom", "unknown").toString();
+            resourcePackHash = props.getOrDefault("resource_pack_hash", "dev").toString();
+        } catch (Exception e) {
+            logger.warn("Failed to load runtime properties", e);
+        }
+    }
 
     @Override
     public @NotNull String version() {
-        return "{MAPMAKER_VERSION}";
+        return version;
     }
 
     @Override
     public @NotNull String commit() {
-        return "{MAPMAKER_COMMIT}";
+        return commit;
     }
 
     @Override
     public @NotNull String minestom() {
-        return "{MINESTOM_VERSION}";
+        return minestom;
     }
 
 
     public @NotNull String resourcePackSha1() {
-        return "{RESOURCE_PACK_SHA}";
+        return resourcePackHash;
     }
 
 }
