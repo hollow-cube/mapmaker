b7771868c46771e68ba1093da5d55835bfe87c25
GreatWyrm <alecmusante@gmail.com>
2023-12-02 11:31:32 -0800

    Add hub spawn command

---
+++ modules/hub/src/main/java/net/hollowcube/mapmaker/hub/command/util/HubSpawnCommand.java
@@ -0,0 +1,23 @@
+package net.hollowcube.mapmaker.hub.command.util;
+
+import net.hollowcube.command.Command;
+import net.hollowcube.command.CommandContext;
+import net.hollowcube.mapmaker.hub.HubServerBase;
+import net.minestom.server.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class HubSpawnCommand extends Command {
+
+    private final HubServerBase hubServerBase;
+
+    public HubSpawnCommand(HubServerBase hubServerBase) {
+        super("spawn");
+
+        this.hubServerBase = hubServerBase;
+        addSyntax(playerOnly(this::handleSpawn));
+    }
+
+    private void handleSpawn(@NotNull Player player, @NotNull CommandContext context) {
+        hubServerBase.teleportToSpawn(player);
+    }
+}
