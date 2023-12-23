8c604710df334c3f3fbee69b8a3c37a678e3ba4b
mworzala <mattheworzala@gmail.com>
2023-12-22 19:47:38 -0500

    feat: DELETE GRADLE YAYAYAYAYAY

--- modules/canvas/impl-standalone/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    implementation(project(":modules:canvas:api"))
-    testImplementation(project(":modules:canvas:api"))
-
-    testImplementation("ch.qos.logback:logback-classic:1.4.5")
-    testImplementation("org.slf4j:jul-to-slf4j:2.0.6")
-
-//    implementation(files("/Users/matt/HeadDatabase-4.17.0.jar"))
-}
--- modules/replay/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-
-    implementation("com.github.mworzala.mc_debug_renderer:minestom:2c354a8e0859b765144d7c629c2a4d62b5f1d220")
-
-}
