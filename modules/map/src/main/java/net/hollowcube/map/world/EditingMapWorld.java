c0422d7b76f30ae054dca6fcae66ee55cc148f93
mworzala <mattheworzala@gmail.com>
2024-02-12 14:34:25 -0500

    Revert "uh oh im worried: enable lighting in hub editing world"
    This reverts commit de7ae70e513493b47d01fab9fce385b7e88971a3.

--- modules/map/src/main/java/net/hollowcube/map/world/EditingMapWorld.java
+++ modules/map/src/main/java/net/hollowcube/map/world/EditingMapWorld.java
@@ -34,14 +34,12 @@ import net.minestom.server.event.player.PlayerUseItemEvent;
 import net.minestom.server.event.trait.InstanceEvent;
 import net.minestom.server.event.trait.PlayerEvent;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.network.packet.server.play.EffectPacket;
 import net.minestom.server.tag.Tag;
 import net.minestom.server.timer.Task;
 import net.minestom.server.utils.time.TimeUnit;
-import net.minestom.server.world.DimensionType;
 import org.jetbrains.annotations.Blocking;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -89,12 +87,12 @@ public class EditingMapWorld implements InternalMapWorld {
         this.map = map;
         this.flags |= FLAG_EDITING;
 
-        if (map.id().equals(MapData.SPAWN_MAP_ID)) {
-            instance = new MapInstance(getDimensionName(), DimensionType.OVERWORLD);
-            instance.setChunkSupplier(LightingChunk::new);
-        } else {
-            instance = new MapInstance(getDimensionName());
-        }
+//        if (map.id().equals(MapData.SPAWN_MAP_ID)) {
+//            instance = new MapInstance(getDimensionName(), DimensionType.OVERWORLD);
+//            instance.setChunkSupplier(LightingChunk::new);
+//        } else {
+        instance = new MapInstance(getDimensionName());
+//        }
         instance.setGenerator(MapGenerators.voidWorld());
         instance.setTag(SELF_TAG, this);
 
