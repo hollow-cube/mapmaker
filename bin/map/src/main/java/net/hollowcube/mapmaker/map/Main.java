472b692569be37cc1b5e28205bc8e994f215e909
Callum Seabrook <callum.seabrook@prevarinite.com>
2024-02-21 21:01:20 +0000

    fix: don't mark map Main as @Blocking

--- bin/map/src/main/java/net/hollowcube/mapmaker/map/Main.java
+++ bin/map/src/main/java/net/hollowcube/mapmaker/map/Main.java
@@ -3,7 +3,6 @@ package net.hollowcube.mapmaker.map;
 import net.hollowcube.mapmaker.map.runtime.MapServerInitializer;
 import org.jetbrains.annotations.Blocking;
 
-@Blocking
 public class Main {
     public static void main(String[] args) {
         MapServerInitializer.run(MapServerRunner::new, args);
