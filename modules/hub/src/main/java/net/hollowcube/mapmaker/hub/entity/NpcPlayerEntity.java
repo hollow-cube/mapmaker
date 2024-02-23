5b95e6c5686664c8d6f30ae748ef4df28158b878
mworzala <mattheworzala@gmail.com>
2024-02-23 09:49:55 -0500

    feat: npc titles, support skin & name & prompt in marker data

--- modules/hub/src/main/java/net/hollowcube/mapmaker/hub/entity/NpcPlayerEntity.java
+++ modules/hub/src/main/java/net/hollowcube/mapmaker/hub/entity/NpcPlayerEntity.java
@@ -1,16 +1,24 @@
 package net.hollowcube.mapmaker.hub.entity;
 
+import net.hollowcube.common.util.FontUtil;
+import net.hollowcube.mapmaker.to_be_refactored.BadSprite;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.*;
+import net.minestom.server.entity.metadata.display.AbstractDisplayMeta;
+import net.minestom.server.instance.Instance;
 import net.minestom.server.network.packet.server.play.*;
 import net.minestom.server.scoreboard.Team;
+import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jglrxavpok.hephaistos.nbt.NBTCompound;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Common logic for player-like NPCs in the hub.
@@ -21,17 +29,48 @@ public class NpcPlayerEntity extends BaseNpcEntity {
             .nameTagVisibility(TeamsPacket.NameTagVisibility.NEVER)
             .build();
 
-    private final String username;
-    private final PlayerSkin skin;
+    private static final BadSprite RMB_SPRITE = Objects.requireNonNull(BadSprite.SPRITE_MAP.get("icon/mouse_right_small"), "icon/mouse_right_small");
+    private static final Component PROMPT_BASE = Component.text("", TextColor.color(0xCCCCCC))
+            .append(Component.text(RMB_SPRITE.fontChar() + FontUtil.computeOffset(3), NamedTextColor.WHITE))
+            .append(Component.text("ᴛᴏ "));
 
-    public NpcPlayerEntity(@NotNull String username, @Nullable PlayerSkin skin) {
-        this(UUID.randomUUID(), username, skin);
+    private static final Tag<PlayerSkin> SKIN_TAG = Tag.Structure("skin", PlayerSkin.class);
+
+    private static int nameCounter = 1;
+
+    private final String fakeUsername = String.valueOf(nameCounter++);
+
+    private final NpcTextModel titleEntity = new NpcTextModel();
+    private final NpcTextModel subtitleEntity = new NpcTextModel();
+
+    public NpcPlayerEntity(@NotNull NBTCompound nbt) {
+        this(UUID.randomUUID(), nbt);
     }
 
-    public NpcPlayerEntity(@NotNull UUID uuid, @NotNull String username, @Nullable PlayerSkin skin) {
+    public NpcPlayerEntity(@NotNull UUID uuid, @NotNull NBTCompound nbt) {
         super(EntityType.PLAYER, uuid);
-        this.username = username;
-        this.skin = skin;
+
+        var titleMeta = titleEntity.getEntityMeta();
+        titleMeta.setBillboardRenderConstraints(AbstractDisplayMeta.BillboardConstraints.CENTER);
+        titleMeta.setTranslation(new Vec(0, 0.3, 0));
+
+        var subtitleMeta = subtitleEntity.getEntityMeta();
+        subtitleMeta.setBillboardRenderConstraints(AbstractDisplayMeta.BillboardConstraints.CENTER);
+        subtitleMeta.setTranslation(new Vec(0, 0.1, 0));
+        subtitleMeta.setScale(new Vec(0.7));
+
+        var title = Objects.requireNonNullElseGet(nbt.getString("name"), () -> Objects.requireNonNullElse(nbt.getString("type"), "mapmaker:unknown"));
+        titleEntity.getEntityMeta().setText(Component.text(title));
+        var prompt = nbt.getString("prompt");
+        if (prompt != null) subtitleEntity.getEntityMeta().setText(PROMPT_BASE.append(Component.text(prompt)));
+    }
+
+    @Override
+    public CompletableFuture<Void> setInstance(@NotNull Instance instance, @NotNull Pos spawnPosition) {
+        return super.setInstance(instance, spawnPosition).thenRun(() -> {
+            titleEntity.setInstance(instance, spawnPosition).thenRun(() -> addPassenger(titleEntity));
+            subtitleEntity.setInstance(instance, spawnPosition).thenRun(() -> addPassenger(subtitleEntity));
+        });
     }
 
     @Override
@@ -54,10 +93,11 @@ public class NpcPlayerEntity extends BaseNpcEntity {
     @Override
     public void updateNewViewer(@NotNull Player player) {
         var properties = new ArrayList<PlayerInfoUpdatePacket.Property>();
-        if (this.skin != null) {
+        var skin = getTag(SKIN_TAG);
+        if (skin != null) {
             properties.add(new PlayerInfoUpdatePacket.Property("textures", skin.textures(), skin.signature()));
         }
-        var entry = new PlayerInfoUpdatePacket.Entry(getUuid(), username, properties, false, 0, GameMode.SURVIVAL, null, null);
+        var entry = new PlayerInfoUpdatePacket.Entry(getUuid(), fakeUsername, properties, false, 0, GameMode.SURVIVAL, null, null);
         player.sendPacket(new PlayerInfoUpdatePacket(PlayerInfoUpdatePacket.Action.ADD_PLAYER, entry));
 
         // Spawn the player entity
@@ -68,7 +108,7 @@ public class NpcPlayerEntity extends BaseNpcEntity {
 
         // Put them on the NPC team to hide their name tag
         final TeamsPacket addPlayerPacket = new TeamsPacket(NPC_TEAM.getTeamName(),
-                new TeamsPacket.AddEntitiesToTeamAction(List.of(this.username)));
+                new TeamsPacket.AddEntitiesToTeamAction(List.of(fakeUsername)));
         sendPacketToViewers(addPlayerPacket);
     }
 
--- modules/hub/src/main/java/net/hollowcube/mapmaker/hub/merchant/MerchantEntity.java
+++ modules/hub/src/main/java/net/hollowcube/mapmaker/hub/merchant/MerchantEntity.java
@@ -1,13 +1,12 @@
 package net.hollowcube.mapmaker.hub.merchant;
 
 import net.hollowcube.mapmaker.hub.entity.NpcPlayerEntity;
-import net.minestom.server.entity.PlayerSkin;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.jglrxavpok.hephaistos.nbt.NBTCompound;
 
 public class MerchantEntity extends NpcPlayerEntity {
-    private static int nameCounter = 1;
 
-    public MerchantEntity(@Nullable PlayerSkin skin) {
-        super(String.valueOf(nameCounter++), skin);
+    public MerchantEntity(@NotNull NBTCompound nbt) {
+        super(nbt);
     }
 }
