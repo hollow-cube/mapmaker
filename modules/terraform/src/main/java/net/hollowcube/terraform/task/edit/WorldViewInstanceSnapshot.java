76de4252b14568e91e293c34ef21b742b282d4d6
mworzala <mattheworzala@gmail.com>
2024-04-01 19:44:29 -0400

    fix: correctly handle out of bounds in border test v2

--- modules/terraform/src/main/java/net/hollowcube/terraform/task/edit/WorldViewInstanceSnapshot.java
+++ modules/terraform/src/main/java/net/hollowcube/terraform/task/edit/WorldViewInstanceSnapshot.java
@@ -7,12 +7,20 @@ import net.minestom.server.snapshot.InstanceSnapshot;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnknownNullability;
 
-record WorldViewInstanceSnapshot(@NotNull Task task, @NotNull InstanceSnapshot snapshot,
-                                 @NotNull WorldBorder border) implements WorldView {
+record WorldViewInstanceSnapshot(
+        @NotNull Task task,
+        @NotNull InstanceSnapshot snapshot,
+        @NotNull WorldBorder border,
+        int worldMin, int worldMax
+) implements WorldView {
+
+    public WorldViewInstanceSnapshot(@NotNull Task task, @NotNull InstanceSnapshot snapshot, @NotNull WorldBorder border) {
+        this(task, snapshot, border, snapshot.dimensionType().getMinY(), snapshot.dimensionType().getMaxY());
+    }
 
     @Override
     public boolean contains(int x, int y, int z) {
-        return WorldViewInstance.fastBorderTest(border, x, y, z);
+        return WorldViewInstance.fastBorderTest(border, x, y, z, worldMin, worldMax);
     }
 
     @Override
