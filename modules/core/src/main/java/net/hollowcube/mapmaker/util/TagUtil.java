32fae316c786292f14932c2a514c3a89672f7be9
mworzala <mattheworzala@gmail.com>
2024-06-29 09:32:39 -0400

    fix: lower requests

--- modules/core/src/main/java/net/hollowcube/mapmaker/util/TagUtil.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/util/TagUtil.java
@@ -17,6 +17,7 @@ public final class TagUtil {
     public static void removeTooltipExtras(@NotNull ItemStack.Builder item) {
         item.remove(ItemComponent.FIREWORKS); // Has flight duration lore
         item.remove(ItemComponent.ENCHANTMENTS);
+        item.remove(ItemComponent.JUKEBOX_PLAYABLE);
         // Note: Just removing attribute modifiers does not remove the armor extras tooltip
         // we actually have to set attributes with nothing for that to happen.
         item.set(ItemComponent.ATTRIBUTE_MODIFIERS, new AttributeList(List.of(), false));
