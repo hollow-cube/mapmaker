d9583117e4f0cdf725a3b07728dc6a68acb41d10
mworzala <mattheworzala@gmail.com>
2024-09-21 19:59:09 -0400

    fix: use posthog

--- modules/core/src/main/java/net/hollowcube/mapmaker/feature/unleash/UnleashConfig.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/feature/unleash/UnleashConfig.java
@@ -7,6 +7,7 @@ import org.spongepowered.configurate.objectmapping.meta.Setting;
 @ConfigSerializable
 public record UnleashConfig(
         boolean enabled,
+        @Setting("use_posthog")
         boolean usePosthog,
         @NotNull String address,
         @NotNull String token,
