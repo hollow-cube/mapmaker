63be3988bb8b70760624b58e4c5340f0cbdd0a44
mworzala <mattheworzala@gmail.com>
2024-11-07 19:31:38 -0500

    fix: correct handling of player inventory slots

--- modules/canvas/impl-standalone/src/main/java/net/hollowcube/canvas/internal/standalone/provider/InventoryViewHost.java
+++ modules/canvas/impl-standalone/src/main/java/net/hollowcube/canvas/internal/standalone/provider/InventoryViewHost.java
@@ -329,14 +329,6 @@ public class InventoryViewHost {
                 return;
             }
 
-            // todo below will return post-inventory rework.
-            // If the click happened inside the player inventory we need to do our weird accounting of anvil being 9 internally
-            if (slot >= wrapper.getSize()) {
-                slot -= wrapper.getSize(); // Now represents the slot starting from the bottom inventory
-                var offset = wrapper.getInventoryType() == InventoryType.ANVIL ? 9 : wrapper.getInventoryType().getSize();
-                slot += offset; // Offset to the bottom of the top inventory (accounting for our weird anvil inventory)
-            }
-
             var allow = wrapper.tryHandleClick(slot, event.getPlayer(), clickType);
             event.setCancelled(!allow);
         }
@@ -428,7 +420,7 @@ public class InventoryViewHost {
 
             // Copy all contents of players current inventory to this one
             var old = player.getInventory();
-            viewers.addAll(old.getViewers()); //todo reenable when updating minestom
+            viewers.addAll(old.getViewers());
             // PlayerInventory
             setCursorItem(old.getCursorItem());
             // AbstractInventory
