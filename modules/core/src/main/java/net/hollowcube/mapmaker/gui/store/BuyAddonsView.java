e9ae3b5e268f440f8284e9354a86613b4930c2ab
SethPRG <sethhargis28@gmail.com>
2024-12-22 00:59:25 -0500

    feat: new art store
    feat: updated player inventory texture
    
    feat: cosmetic GUI
    
    feat: confirm GUI for making add-ons purchases
    
    feat: finalized store GUI

--- modules/core/src/main/java/net/hollowcube/mapmaker/gui/store/BuyAddonsView.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/gui/store/BuyAddonsView.java
@@ -3,7 +3,6 @@ package net.hollowcube.mapmaker.gui.store;
 import com.google.gson.JsonObject;
 import net.hollowcube.canvas.Label;
 import net.hollowcube.canvas.Switch;
-import net.hollowcube.canvas.Text;
 import net.hollowcube.canvas.View;
 import net.hollowcube.canvas.annotation.Action;
 import net.hollowcube.canvas.annotation.ContextObject;
@@ -11,6 +10,7 @@ import net.hollowcube.canvas.annotation.Outlet;
 import net.hollowcube.canvas.annotation.OutletGroup;
 import net.hollowcube.canvas.internal.Context;
 import net.hollowcube.mapmaker.backpack.PlayerBackpack;
+import net.hollowcube.mapmaker.gui.common.ConfirmAction;
 import net.hollowcube.mapmaker.perm.PermManager;
 import net.hollowcube.mapmaker.player.PlayerDataV2;
 import net.hollowcube.mapmaker.player.PlayerService;
@@ -34,12 +34,10 @@ public class BuyAddonsView extends View {
 
     private @Outlet("terraform_switch") Switch terraformSwitch;
     private @Outlet("map_slots_switch") Switch mapSlotsSwitch;
-    private @Outlet("map_slots_cost") Text mapSlotsCost;
+    private @Outlet("map_slots_cost") Switch mapSlotsCost;
     // Boosts
-    // Folders
     private @Outlet("map_size_switch") Switch mapSizeSwitch;
-    private @Outlet("map_size_cost") Text mapSizeCost;
-    // Personal world
+    private @Outlet("map_size_cost") Switch mapSizeCost;
 
     private @OutletGroup("buy_.+") Label[] buyableEntries;
 
@@ -50,28 +48,30 @@ public class BuyAddonsView extends View {
 
         if (!ShopUpgradeCache.has(player, ShopUpgrade.MAP_SLOT_3, true)) {
             mapSlotsSwitch.setOption(0);
-            mapSlotsCost.setText(ShopUpgrade.MAP_SLOT_3.cubits() + "c");
+            mapSlotsCost.setOption(0);
         } else if (!ShopUpgradeCache.has(player, ShopUpgrade.MAP_SLOT_4, true)) {
             mapSlotsSwitch.setOption(1);
-            mapSlotsCost.setText(ShopUpgrade.MAP_SLOT_4.cubits() + "c");
+            mapSlotsCost.setOption(1);
         } else if (!ShopUpgradeCache.has(player, ShopUpgrade.MAP_SLOT_5, true)) {
             mapSlotsSwitch.setOption(2);
-            mapSlotsCost.setText(ShopUpgrade.MAP_SLOT_5.cubits() + "c");
+            mapSlotsCost.setOption(2);
         } else {
             mapSlotsSwitch.setOption(3);
+            mapSlotsCost.setOption(3);
         }
 
         if (!ShopUpgradeCache.has(player, ShopUpgrade.MAP_SIZE_2, true)) {
             mapSizeSwitch.setOption(0);
-            mapSizeCost.setText(ShopUpgrade.MAP_SIZE_2.cubits() + "c");
+            mapSizeCost.setOption(0);
         } else if (!ShopUpgradeCache.has(player, ShopUpgrade.MAP_SIZE_3, true)) {
             mapSizeSwitch.setOption(1);
-            mapSizeCost.setText(ShopUpgrade.MAP_SIZE_3.cubits() + "c");
+            mapSizeCost.setOption(1);
         } else if (!ShopUpgradeCache.has(player, ShopUpgrade.MAP_SIZE_4, true)) {
             mapSizeSwitch.setOption(2);
-            mapSizeCost.setText(ShopUpgrade.MAP_SIZE_4.cubits() + "c");
+            mapSizeCost.setOption(2);
         } else {
             mapSizeSwitch.setOption(3);
+            mapSizeCost.setOption(3);
         }
 
         for (var entry : buyableEntries) {
@@ -109,8 +109,6 @@ public class BuyAddonsView extends View {
 
     //todo boosts
 
-    //todo folders
-
     @Action(value = "buy_map_size_2", async = true)
     private void handleMapSizeLarge() {
         submitUpgradePurchase(ShopUpgrade.MAP_SIZE_2);
@@ -142,8 +140,15 @@ public class BuyAddonsView extends View {
             return;
         }
 
-        //todo this should have a confirm gui, maybe with some extra details
+        pushView(context -> new ConfirmAction(context, () -> tryUpgradePurchase(upgrade),
+                Component.translatable("purchase this upgrade for " + upgrade.cubits() + " Cubits")));
+
+    }
+
+    private void tryUpgradePurchase(@NotNull ShopUpgrade upgrade) {
         try {
+            var playerData = PlayerDataV2.fromPlayer(player);
+
             var meta = new JsonObject();
             meta.addProperty("source", "ingame/store");
             playerService.buyUpgrade(playerData.id(), upgrade.name().toLowerCase(Locale.ROOT), upgrade.cubits(), meta);
