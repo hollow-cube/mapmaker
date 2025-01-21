01f4293282637aca746dcf5c0208b47c0b033009
Gravy Boat <gravy@thatgravyboat.tech>
2025-01-21 14:38:36 -0330

    Make text display move to face of block

--- modules/map/src/main/java/net/hollowcube/mapmaker/map/feature/edit/item/DisplayEntityItem.java
+++ modules/map/src/main/java/net/hollowcube/mapmaker/map/feature/edit/item/DisplayEntityItem.java
@@ -1,11 +1,13 @@
 package net.hollowcube.mapmaker.map.feature.edit.item;
 
+import net.hollowcube.common.util.OpUtils;
 import net.hollowcube.mapmaker.map.MapWorld;
 import net.hollowcube.mapmaker.map.entity.impl.DisplayEntity;
 import net.hollowcube.mapmaker.map.gui.displayentity.AbstractEditDisplayView;
 import net.hollowcube.mapmaker.map.gui.displayentity.CreateDisplayView;
 import net.hollowcube.mapmaker.map.item.handler.ItemHandler;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.item.Material;
 import org.jetbrains.annotations.NotNull;
 
@@ -34,10 +36,10 @@ public class DisplayEntityItem extends ItemHandler {
 
         if (selectedId != null) {
             var selectedEntity = world.instance().getEntityByUuid(selectedId);
-            if (selectedEntity != null) {
-                if (player.isSneaking() && click.placePosition() != null) {
-                    selectedEntity.teleport(Pos.fromPoint(click.placePosition().add(0.5)));
-                } else if (selectedEntity instanceof DisplayEntity display) {
+            if (selectedEntity instanceof DisplayEntity display) {
+                if (player.isSneaking()) {
+                    placeOnBlock(display, click);
+                } else {
                     world.server().showView(player, context -> AbstractEditDisplayView.create(context, display));
                 }
             } else {
@@ -48,4 +50,18 @@ public class DisplayEntityItem extends ItemHandler {
         }
     }
 
+    private void placeOnBlock(DisplayEntity display, Click click) {
+        var direction = OpUtils.map(click.face(), BlockFace::toDirection);
+        if (click.placePosition() == null || click.blockPosition() == null || direction == null) return;
+        if (display instanceof DisplayEntity.Text && direction.horizontal()) {
+            var pos = click.placePosition().add(Math.abs(direction.normalZ() * 0.5), 0.5, Math.abs(direction.normalX() * 0.5));
+            if (direction.positive()) {
+                display.teleport(Pos.fromPoint(pos));
+            } else {
+                display.teleport(Pos.fromPoint(pos.add(-direction.normalX(), 0, -direction.normalZ())));
+            }
+        } else {
+            display.teleport(Pos.fromPoint(click.placePosition().add(0.5)));
+        }
+    }
 }
