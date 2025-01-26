d148d4d078938c1cee58ffeed78d443149fe6e54
Gravy Boat <gravy@thatgravyboat.tech>
2025-01-26 12:23:28 -0330

    Ensure signs are atleast 4 lines
    closes #507

---
+++ modules/common/src/main/java/net/hollowcube/common/util/CollectionUtil.java
@@ -0,0 +1,20 @@
+package net.hollowcube.common.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class CollectionUtil {
+
+    @SafeVarargs
+    public static <T>List<T> copyWithMinSize(int minSize, Supplier<T> fallback, T... elements) {
+        if (elements.length > minSize) return List.of(elements);
+        List<T> list = new ArrayList<>(minSize);
+        list.addAll(Arrays.asList(elements));
+        for (int i = elements.length; i < minSize; i++) {
+            list.add(fallback.get());
+        }
+        return List.copyOf(list);
+    }
+}
