d0a94e293db135d462a515d484cacd573bccc8ad
ThatGravyBoat <gravy@thatgravyboat.tech>
2025-01-27 12:37:40 -0330

    Make builder menu items conditional by map (#563)
    * Make builder menu items conditional by map
    
    * Add NotNull to all types

---
+++ modules/map/src/main/java/net/hollowcube/mapmaker/map/gui/buildermenu/BuilderMenuTab.java
@@ -0,0 +1,43 @@
+package net.hollowcube.mapmaker.map.gui.buildermenu;
+
+import net.hollowcube.canvas.Pagination;
+import net.hollowcube.canvas.View;
+import net.hollowcube.canvas.annotation.Action;
+import net.hollowcube.canvas.annotation.Outlet;
+import net.hollowcube.canvas.internal.Context;
+import net.hollowcube.mapmaker.map.MapWorld;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuilderMenuTab extends View {
+
+    private @Outlet("entries") Pagination entries;
+
+    private final List<BuilderMenuTabItems.Item> items = new ArrayList<>();
+
+    public BuilderMenuTab(@NotNull Context context) {
+        super(context);
+    }
+
+    @Action("entries")
+    private void fetchEntries(@NotNull Pagination.PageRequest<BuilderMenuEntry> request) {
+        var player = request.context().player();
+        var map = MapWorld.forPlayerOptional(player);
+
+        var entries = new ArrayList<BuilderMenuEntry>();
+        for (var item : items) {
+            if (map == null || item.isVisible(map, player)) {
+                entries.add(new BuilderMenuEntry(request.context(), item));
+            }
+        }
+        request.respond(entries, false);
+    }
+
+    public void setItems(@NotNull BuilderMenuTabItems.Item... items) {
+        this.items.clear();
+        this.items.addAll(List.of(items));
+        this.entries.reset();
+    }
+}
