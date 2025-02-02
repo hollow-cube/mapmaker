8e60a2618594a23d86c55ef007b0014cd9eae8d8
Gravy Boat <gravy@thatgravyboat.tech>
2025-02-02 16:28:45 -0330

    automatically turn axe to terraform wand
    closes #547

--- modules/core/src/main/java/net/hollowcube/mapmaker/cosmetic/CosmeticInventoryHandler.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/cosmetic/CosmeticInventoryHandler.java
@@ -1,16 +1,14 @@
 package net.hollowcube.mapmaker.cosmetic;
 
 import net.hollowcube.canvas.internal.Controller;
+import net.hollowcube.mapmaker.event.PlayerGiveCreativeItemEvent;
 import net.hollowcube.mapmaker.gui.store.CosmeticView;
 import net.hollowcube.mapmaker.misc.MiscFunctionality;
 import net.hollowcube.mapmaker.player.PlayerDataV2;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.GameMode;
-import net.minestom.server.entity.Player;
 import net.minestom.server.event.inventory.InventoryPreClickEvent;
 import net.minestom.server.item.ItemStack;
-import net.minestom.server.listener.CreativeInventoryActionListener;
-import net.minestom.server.network.packet.client.play.ClientCreativeInventoryActionPacket;
 import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,8 +19,7 @@ public class CosmeticInventoryHandler {
     public static void init(@NotNull Controller guiController) {
         var globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(InventoryPreClickEvent.class, event -> handleInventoryCosmeticSelector(guiController, event));
-        var packetListenerManager = MinecraftServer.getPacketListenerManager();
-        packetListenerManager.setPlayListener(ClientCreativeInventoryActionPacket.class, (packet, player) -> creativeClickListener(guiController, packet, player));
+        globalEventHandler.addListener(PlayerGiveCreativeItemEvent.class, CosmeticInventoryHandler::creativeClickListener);
     }
 
     private static void handleInventoryCosmeticSelector(@NotNull Controller guiController, @NotNull InventoryPreClickEvent event) {
@@ -56,32 +53,30 @@ public class CosmeticInventoryHandler {
 
     private static final Tag<Integer> CREATIVE_LAST_SLOT_WIPE_HACK = Tag.Integer("creative_last_slot_wipe_hack").defaultValue(0);
 
-    private static void creativeClickListener(@NotNull Controller guiController, ClientCreativeInventoryActionPacket packet, Player player) {
-        if (player.getGameMode() != GameMode.CREATIVE) return;
-        var item = packet.item();
+    private static void creativeClickListener(@NotNull PlayerGiveCreativeItemEvent event) {
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) return;
+        var item = event.item();
 
         // If trying to set any slot to a cosmetic item
         if (item.hasTag(Cosmetic.COSMETIC_TAG)) {
-            player.getInventory().update();
+            event.getPlayer().getInventory().update();
             return;
         }
 
         // Only care about cosmetic slots
         // We need to exclude any clicks that are setting it to a leather horse armor. The creative client handler resets it to the cmd item when reopening to sync.
-        var cosmeticType = COSMETIC_SLOT_MAP.get(packet.slot());
-        if (cosmeticType == null || !item.isAir()) {
-            // Defer to the Minestom handling
-            CreativeInventoryActionListener.listener(packet, player);
-            return;
-        }
+        var cosmeticType = COSMETIC_SLOT_MAP.get(event.slot());
+        if (cosmeticType == null || !item.isAir()) return; // Defer to the Minestom handling
 
         // Reset the inventory state
-        MiscFunctionality.applyCosmetics(player, PlayerDataV2.fromPlayer(player));
-        player.getInventory().setCursorItem(ItemStack.AIR);
-        player.getInventory().update();
+        MiscFunctionality.applyCosmetics(event.getPlayer(), PlayerDataV2.fromPlayer(event.getPlayer()));
+        event.getPlayer().getInventory().setCursorItem(ItemStack.AIR);
+        event.getPlayer().getInventory().update();
 
         // Show the cosmetic type selector
 //        guiController.show(player, c -> new CosmeticView(c, cosmeticType));
+
+        event.setCancelled(true);
     }
 
 }
---
+++ modules/core/src/main/java/net/hollowcube/mapmaker/event/PlayerGiveCreativeItemEvent.java
@@ -0,0 +1,65 @@
+package net.hollowcube.mapmaker.event;
+
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.EventDispatcher;
+import net.minestom.server.event.trait.CancellableEvent;
+import net.minestom.server.event.trait.PlayerInstanceEvent;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.listener.CreativeInventoryActionListener;
+import net.minestom.server.network.packet.client.play.ClientCreativeInventoryActionPacket;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
+public final class PlayerGiveCreativeItemEvent implements PlayerInstanceEvent, CancellableEvent {
+
+    private final @NotNull Player player;
+    private final @NotNull ItemStack item;
+    private final short slot;
+
+    private boolean cancelled;
+
+    public PlayerGiveCreativeItemEvent(
+            @NotNull Player player,
+            @NotNull ItemStack item,
+            short slot
+    ) {
+        this.player = player;
+        this.item = item;
+        this.slot = slot;
+    }
+
+    @Override
+    public @NotNull Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    public @NotNull ItemStack item() {
+        return item;
+    }
+
+    public short slot() {
+        return slot;
+    }
+
+    @ApiStatus.Internal
+    public static void post(ClientCreativeInventoryActionPacket packet, Player player) {
+        var event = new PlayerGiveCreativeItemEvent(player, packet.item(), packet.slot());
+        EventDispatcher.call(event);
+
+        if (!event.isCancelled()) {
+            CreativeInventoryActionListener.listener(packet, player);
+        }
+    }
+
+
+}
