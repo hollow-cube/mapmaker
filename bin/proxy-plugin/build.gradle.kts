3828073b2e0535523c75293e45ce7a18e2a4c05e
mworzala <mattheworzala@gmail.com>
2025-05-07 22:33:36 -0400

    chore: stop shadowing proxy plugin, disconnect if session removed

--- bin/proxy-plugin/build.gradle.kts
+++ bin/proxy-plugin/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    // Not actually a binary but we use java-binary so that shadowjar is applied.
-    id("mapmaker.java-binary")
+    id("mapmaker.java-library")
 }
 
 repositories {
@@ -12,17 +11,5 @@ dependencies {
     implementation(libs.velocity.api) {
         exclude(group = "net.kyori")
     }
-    implementation(libs.kafka)
-
     implementation(libs.bundles.adventure)
-}
-
-application {
-    mainClass = "not.a.main"
-}
-
-tasks.shadowJar {
-//    dependencies {
-//        include("org.apache.kafka:kafka-clients")
-//    }
 }
