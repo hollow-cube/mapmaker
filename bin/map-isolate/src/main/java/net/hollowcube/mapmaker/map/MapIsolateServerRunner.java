57eaad2c796782008cc79463c96e93784910fc7d
mworzala <mattheworzala@gmail.com>
2025-05-07 22:25:10 -0400

    chore: more setup for tilt and improved player tracking & disconnect handling in proxy.

--- bin/map-isolate/src/main/java/net/hollowcube/mapmaker/map/MapIsolateServerRunner.java
+++ bin/map-isolate/src/main/java/net/hollowcube/mapmaker/map/MapIsolateServerRunner.java
@@ -4,20 +4,24 @@ import net.hollowcube.command.CommandManager;
 import net.hollowcube.common.ServerRuntime;
 import net.hollowcube.common.util.FutureUtil;
 import net.hollowcube.common.util.Uuids;
+import net.hollowcube.mapmaker.ExceptionReporter;
 import net.hollowcube.mapmaker.config.ConfigLoaderV3;
 import net.hollowcube.mapmaker.map.feature.FeatureList;
 import net.hollowcube.mapmaker.map.hdb.HeadDatabase;
 import net.hollowcube.mapmaker.map.runtime.AbstractMapServer;
 import net.hollowcube.mapmaker.map.runtime.MapAllocator;
-import net.hollowcube.mapmaker.map.runtime.NoopServerBridge;
 import net.hollowcube.mapmaker.map.runtime.ServerBridge;
 import net.hollowcube.mapmaker.map.util.MapPlayerImplImpl;
 import net.hollowcube.mapmaker.map.world.AbstractMapMakerMapWorld;
 import net.hollowcube.mapmaker.map.world.PlayingMapWorld;
+import net.hollowcube.mapmaker.misc.ProxySupport;
 import net.hollowcube.mapmaker.misc.ResourcePackManager;
+import net.hollowcube.mapmaker.player.JoinHubRequest;
+import net.hollowcube.mapmaker.player.PlayerDataV2;
 import net.hollowcube.mapmaker.session.Presence;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.event.player.PlayerDisconnectEvent;
@@ -27,16 +31,25 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.UUID;
+
 public class MapIsolateServerRunner extends AbstractMapServer {
     private static final Logger logger = LoggerFactory.getLogger(MapIsolateServerRunner.class);
 
-    public static final String MAP_ID = "f363bbd3-db9d-4957-bcc6-11b0ffa3a456";
+    private final String mapId;
 
     private AbstractMapMakerMapWorld world;
     private FeatureList features;
 
     public MapIsolateServerRunner(@NotNull ConfigLoaderV3 config) {
         super(config);
+
+        if (IsolateMain.args.length < 1)
+            throw new IllegalArgumentException("Map ID must be provided as the last argument");
+        this.mapId = UUID.fromString(IsolateMain.args[IsolateMain.args.length - 1]).toString();
+        System.out.println("Map ID: " + this.mapId);
+        System.out.println(Arrays.toString(IsolateMain.args));
 
         MinecraftServer.getGlobalEventHandler().addChild(EventNode.all("map-init")
                 .addListener(AsyncPlayerConfigurationEvent.class, this::handleConfigPhase)
@@ -56,7 +69,26 @@ public class MapIsolateServerRunner extends AbstractMapServer {
 
     @Override
     protected @NotNull ServerBridge createBridge() {
-        return new NoopServerBridge();
+        return new ServerBridge() {
+            @Override
+            public void joinMap(@NotNull Player player, @NotNull String mapId, @NotNull JoinMapState joinMapState, @NotNull String source) {
+                player.sendMessage("todo");
+                return;
+            }
+
+            @Override
+            public void joinHub(@NotNull Player player) {
+                try {
+                    var playerData = PlayerDataV2.fromPlayer(player);
+                    var res = sessionService().joinHubV2(new JoinHubRequest(playerData.id()));
+                    logger.info("join hub result: {}", res);
+                    ProxySupport.transfer(player, res.serverClusterIp());
+                } catch (Exception e) {
+                    ExceptionReporter.reportException(e, player);
+                    player.sendMessage(Component.text("An error occurred while trying to return to the hub. Please try again later."));
+                }
+            }
+        };
     }
 
     @Override
@@ -79,7 +111,7 @@ public class MapIsolateServerRunner extends AbstractMapServer {
         shutdowner().queue("features", features::close);
 
         try {
-            var map = mapService().getMap(Uuids.ZERO, MAP_ID);
+            var map = mapService().getMap(Uuids.ZERO, this.mapId);
             world = allocator().allocateDirect(map, PlayingMapWorld.CTOR);
             shutdowner().queue("world", () -> world.close(null));
 
@@ -101,7 +133,7 @@ public class MapIsolateServerRunner extends AbstractMapServer {
 
             logger.info("configuring player {}", player.getUuid());
             var instanceId = ServerRuntime.getRuntime().hostname();
-            var presence = new Presence(Presence.TYPE_MAPMAKER_MAP, "playing", instanceId, MAP_ID);
+            var presence = new Presence(Presence.TYPE_MAPMAKER_MAP, "playing", instanceId, this.mapId);
             transferPlayerSession(player, presence);
 
             // Ensure resource pack was applied before allowing the player in
