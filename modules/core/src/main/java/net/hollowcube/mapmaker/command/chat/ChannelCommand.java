78788c8292f448cb94c4e90ea2f7b0847068ebbe
Gravy Boat <gravy@thatgravyboat.tech>
2025-06-18 00:15:15 -0230

    feat: staff chat and shortcut chat commands

---
+++ modules/core/src/main/java/net/hollowcube/mapmaker/command/chat/ChannelCommand.java
@@ -0,0 +1,60 @@
+package net.hollowcube.mapmaker.command.chat;
+
+import net.hollowcube.mapmaker.chat.ChatMessageListener;
+import net.hollowcube.mapmaker.command.CommandCategories;
+import net.hollowcube.mapmaker.command.arg.CoreArgument;
+import net.hollowcube.mapmaker.map.MapService;
+import net.hollowcube.mapmaker.perm.PermManager;
+import net.hollowcube.mapmaker.perm.PlatformPerm;
+import net.hollowcube.mapmaker.session.SessionManager;
+import net.hollowcube.mapmaker.temp.ClientChatMessageData;
+import org.jetbrains.annotations.NotNull;
+
+public class ChannelCommand extends AbstractChatCommand {
+
+    private ChannelCommand(
+            @NotNull SessionManager sessions, @NotNull MapService maps, @NotNull ChatMessageListener messages,
+            @NotNull String channel, @NotNull String name, @NotNull String... aliases
+    ) {
+        super(sessions, maps, messages, name, aliases);
+
+        this.description = "Send a message to a specific chat channel.";
+        this.category = CommandCategories.SOCIAL;
+
+        var arg = CoreArgument.Message("message").description("The message content to send in the chat.");
+
+        addSyntax(playerOnly(
+                (player, ctx) -> this.handle(player, channel, ctx.get(arg))
+            ),
+                arg
+        );
+    }
+
+    public static class Global extends ChannelCommand {
+        public Global(@NotNull SessionManager sessions, @NotNull MapService maps, @NotNull ChatMessageListener messages) {
+            super(sessions, maps, messages, ClientChatMessageData.CHANNEL_GLOBAL, "gc");
+        }
+    }
+
+    public static class Local extends ChannelCommand {
+        public Local(@NotNull SessionManager sessions, @NotNull MapService maps, @NotNull ChatMessageListener messages) {
+            super(sessions, maps, messages, ClientChatMessageData.CHANNEL_LOCAL, "lc");
+        }
+    }
+
+    public static class Reply extends ChannelCommand {
+        public Reply(@NotNull SessionManager sessions, @NotNull MapService maps, @NotNull ChatMessageListener messages) {
+            super(sessions, maps, messages, ClientChatMessageData.CHANNEL_REPLY, "replay", "r");
+        }
+    }
+
+    public static class Staff extends ChannelCommand {
+        public Staff(@NotNull SessionManager sessions, @NotNull MapService maps, @NotNull ChatMessageListener messages, @NotNull PermManager permissions) {
+            super(sessions, maps, messages, ClientChatMessageData.CHANNEL_STAFF, "sc");
+
+            setCondition(permissions.createPlatformCondition2(PlatformPerm.MAP_ADMIN));
+        }
+    }
+
+
+}
