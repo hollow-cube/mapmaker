018025760f7af2bc1038bbe4b2cdb80bb00657c7
Gravy Boat <gravy@thatgravyboat.tech>
2025-06-19 09:01:49 -0230

    fix: tools left click not working when tool is set to not break blocks

--- modules/common/src/main/java/net/hollowcube/common/events/EventExtensions.java
+++ modules/common/src/main/java/net/hollowcube/common/events/EventExtensions.java
@@ -1,10 +1,7 @@
 package net.hollowcube.common.events;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.network.packet.client.play.ClientCreativeInventoryActionPacket;
-import net.minestom.server.network.packet.client.play.ClientStatusPacket;
-import net.minestom.server.network.packet.client.play.ClientUpdateSignPacket;
-import net.minestom.server.network.packet.client.play.ClientVehicleMovePacket;
+import net.minestom.server.network.packet.client.play.*;
 
 public final class EventExtensions {
 
@@ -18,5 +15,6 @@ public final class EventExtensions {
         packets.setPlayListener(ClientVehicleMovePacket.class, PlayerMoveVehicleEvent::post);
         packets.setPlayListener(ClientUpdateSignPacket.class, UpdateSignTextEvent::post);
         packets.setPlayListener(ClientStatusPacket.class, RequestStatsEvent::post);
+        packets.setPlayListener(ClientPlayerDiggingPacket.class, PlayerHitBlockEvent::post);
     }
 }
