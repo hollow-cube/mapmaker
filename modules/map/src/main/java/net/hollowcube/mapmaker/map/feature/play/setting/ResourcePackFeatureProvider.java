3f9740af5f73445a7423d59067bced23b2cd1ea8
Gravy Boat <gravy@thatgravyboat.tech>
2025-07-29 09:34:52 -0230

    requested changes.

--- modules/map/src/main/java/net/hollowcube/mapmaker/map/feature/play/setting/ResourcePackFeatureProvider.java
+++ modules/map/src/main/java/net/hollowcube/mapmaker/map/feature/play/setting/ResourcePackFeatureProvider.java
@@ -2,6 +2,7 @@ package net.hollowcube.mapmaker.map.feature.play.setting;
 
 
 import com.google.auto.service.AutoService;
+import net.hollowcube.common.lang.LanguageProviderV2;
 import net.hollowcube.mapmaker.map.MapSettings;
 import net.hollowcube.mapmaker.map.MapWorld;
 import net.hollowcube.mapmaker.map.feature.FeatureProvider;
@@ -13,21 +14,26 @@ import net.minestom.server.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
+import java.util.UUID;
 
 @AutoService(FeatureProvider.class)
 public class ResourcePackFeatureProvider implements FeatureProvider {
 
+    private static final UUID PACK_UUID = MapWorldHelpers.MAP_WORLD_RESOURCE_PACK_UUID;
+
     @Override
     public void configurePlayer(@NotNull MapWorld world, @NotNull Player player) {
         var pack = world.map().getSetting(MapSettings.RESOURCE_PACK);
-        if (pack.isEmpty()) return;
+        if (pack.isEmpty()) {
+            player.removeResourcePacks(PACK_UUID);
+        } else {
+            var url = String.format("https://hollowcube-resource-pack.s3.amazonaws.com/%s.zip", pack);
+            var request = ResourcePackRequest.resourcePackRequest()
+                    .packs(ResourcePackInfo.resourcePackInfo(PACK_UUID, URI.create(url), pack))
+                    .prompt(LanguageProviderV2.translate(Component.translatable("map.join.resource_pack.prompt")))
+                    .required(true);
 
-        var url = String.format("https://hollowcube-resource-pack.s3.amazonaws.com/%s.zip", pack);
-        var request = ResourcePackRequest.resourcePackRequest()
-                .packs(ResourcePackInfo.resourcePackInfo(MapWorldHelpers.MAP_WORLD_RESOURCE_PACK_UUID, URI.create(url), pack))
-                .prompt(Component.text("Download the required map resource pack to play this map.")) // TODO should probably have better text
-                .required(true);
-
-        player.sendResourcePacks(request);
+            player.sendResourcePacks(request);
+        }
     }
 }
