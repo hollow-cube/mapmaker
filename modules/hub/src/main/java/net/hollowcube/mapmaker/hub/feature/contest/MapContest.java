2c9379cec7e96c56df2a05185c2f752e1d7cd71f
mworzala <mattheworzala@gmail.com>
2025-08-17 18:18:48 -0400

    extend contest by a day

--- modules/hub/src/main/java/net/hollowcube/mapmaker/hub/feature/contest/MapContest.java
+++ modules/hub/src/main/java/net/hollowcube/mapmaker/hub/feature/contest/MapContest.java
@@ -40,7 +40,7 @@ public class MapContest implements HubFeature {
     public static final LocalDateTime START_DATE = LocalDateTime.parse(
             "2025-07-19T12:00:00-04:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     public static final LocalDateTime END_DATE = LocalDateTime.parse(
-            "2025-08-18T00:00:00-04:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            "2025-08-19T00:00:00-04:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
 
     private static final Pos TEXT_SPAWN_POS = new Pos(-37.5, 42 + 7, 54.5, 0, 0);
     private static final Pos NPC_SPAWN_POS = new Pos(-37.5, 40, 47.5, 180, 10);
