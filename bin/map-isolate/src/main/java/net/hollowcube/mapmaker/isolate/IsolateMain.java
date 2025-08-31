f6f3fe516b33d2dddd63199eb02f7a966a903d9d
mworzala <mattheworzala@gmail.com>
2025-08-30 21:35:44 -0400

    fix: no echo in distroless :)

--- bin/map-isolate/src/main/java/net/hollowcube/mapmaker/isolate/IsolateMain.java
+++ bin/map-isolate/src/main/java/net/hollowcube/mapmaker/isolate/IsolateMain.java
@@ -6,6 +6,11 @@ public class IsolateMain {
     public static String[] args;
 
     public static void main(String[] args) {
+        if (args.length > 0 && "noop".equals(args[1])) {
+            System.out.println("Exiting because of noop argument.");
+            System.exit(0);
+        }
+
         IsolateMain.args = args;
         MapServerInitializer.run(MapIsolateServer::new, args);
     }
