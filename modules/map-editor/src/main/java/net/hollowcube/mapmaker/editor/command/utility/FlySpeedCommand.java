34ae7911fcd4b47d7d0f92451d9b62d03d84773c
Gravy Boat <gravy@thatgravyboat.tech>
2025-10-01 04:39:15 -0230

    fix: flyspeed not usable in Building maps

--- modules/map-editor/src/main/java/net/hollowcube/mapmaker/editor/command/utility/FlySpeedCommand.java
+++ modules/map-editor/src/main/java/net/hollowcube/mapmaker/editor/command/utility/FlySpeedCommand.java
@@ -4,6 +4,7 @@ import net.hollowcube.command.CommandContext;
 import net.hollowcube.command.arg.Argument;
 import net.hollowcube.command.dsl.CommandDsl;
 import net.hollowcube.common.components.TranslatableBuilder;
+import net.hollowcube.mapmaker.runtime.building.command.BuildingConditions;
 import net.minestom.server.entity.Player;
 
 import static net.hollowcube.command.CommandCondition.or;
@@ -13,16 +14,19 @@ import static net.hollowcube.mapmaker.editor.command.utility.FlyCommand.playingO
 public class FlySpeedCommand extends CommandDsl {
     private static final float DEFAULT_SPEED = 0.05f;
 
-    private final Argument<Float> speedArg = Argument.Float("speed").clamp(0f, 10f)
+    private final Argument<Float> speedArg = Argument.Float("flyspeed").clamp(0f, 10f)
             .description("How fast to fly (from 0 to 10)");
-    //            .errorHandler((sender, context) -> sender.sendMessage("invalid fly speed todo: "));
 
     public FlySpeedCommand() {
         super("flyspeed");
 
         description = "Changes how fast you fly when building";
 
-        setCondition(or(builderOnly(), playingOrSpectatingFilter()));
+        setCondition(or(
+                builderOnly(),
+                playingOrSpectatingFilter(),
+                BuildingConditions.buildingWorld()
+        ));
 
         addSyntax(playerOnly(this::handleSetFlySpeed), speedArg);
     }
--- modules/map-runtime/src/main/java/net/hollowcube/mapmaker/runtime/building/BuildingMapWorld.java
+++ modules/map-runtime/src/main/java/net/hollowcube/mapmaker/runtime/building/BuildingMapWorld.java
@@ -3,6 +3,7 @@ package net.hollowcube.mapmaker.runtime.building;
 import net.hollowcube.mapmaker.map.AbstractMapWorld;
 import net.hollowcube.mapmaker.map.MapData;
 import net.hollowcube.mapmaker.map.MapServer;
+import net.hollowcube.mapmaker.map.MapWorld;
 import net.hollowcube.mapmaker.map.util.EventUtil;
 import net.hollowcube.mapmaker.misc.BossBars;
 import net.hollowcube.mapmaker.runtime.item.MapDetailsItem;
@@ -16,6 +17,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class BuildingMapWorld extends AbstractMapWorld<BuildingState, BuildingMapWorld> {
+
+    public static @Nullable BuildingMapWorld forPlayer(Player player) {
+        return MapWorld.forPlayer(BuildingMapWorld.class, player);
+    }
 
     public BuildingMapWorld(MapServer server, MapData map) {
         super(server, map, makeMapInstance(map, 'b'), BuildingState.class);
