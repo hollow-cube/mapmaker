ad7d16ad02ba4156a3ddb503d86f1161fee950a7
mworzala <mattheworzala@gmail.com>
2025-11-25 21:05:18 -0500

    feat: snowfall particle

--- modules/core/src/main/java/net/hollowcube/mapmaker/cosmetic/Particles.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/cosmetic/Particles.java
@@ -2,6 +2,7 @@ package net.hollowcube.mapmaker.cosmetic;
 
 import net.hollowcube.mapmaker.backpack.Rarity;
 import net.hollowcube.mapmaker.cosmetic.impl.particle.DefaultParticleImpl;
+import net.hollowcube.mapmaker.cosmetic.impl.particle.SnowfallParticleImpl;
 import net.minestom.server.particle.Particle;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,9 +12,14 @@ public final class Particles {
     public static Cosmetic BUBBLE = builder("bubble").rarity(Rarity.COMMON).build();
     public static Cosmetic NOTE = builder("note").rarity(Rarity.COMMON).build();
     public static Cosmetic CHERRY_LEAVES = builder("cherry_leaves")
-            .rarity(Rarity.COMMON)
-            .impl(DefaultParticleImpl.of(Particle.CHERRY_LEAVES, 0.5f))
-            .build();
+        .rarity(Rarity.COMMON)
+        .impl(DefaultParticleImpl.of(Particle.CHERRY_LEAVES, 0.5f))
+        .build();
+    public static Cosmetic SNOWFALL = builder("snowfall")
+        .tags(CosmeticTag.LIMITED_TIME)
+        .rarity(Rarity.LEGENDARY)
+        .impl(SnowfallParticleImpl::new)
+        .build();
 
     public static @NotNull Cosmetic.Builder builder(@NotNull String id) {
         return new Cosmetic.Builder(CosmeticType.PARTICLE, id);
