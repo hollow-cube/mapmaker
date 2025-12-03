52c555a7213597d3baaa88ab0c6567661f196fc2
mworzala <mattheworzala@gmail.com>
2025-12-02 21:30:43 -0500

    chore: remove temp model logic

--- bin/hub/src/main/java/net/hollowcube/mapmaker/multipart/animatedjava/package-info.java
+++
@@ -1,4 +0,0 @@
-@NotNullByDefault
-package net.hollowcube.mapmaker.multipart.animatedjava;
-
-import org.jetbrains.annotations.NotNullByDefault;
--- bin/hub/src/main/java/net/hollowcube/mapmaker/multipart/package-info.java
+++
@@ -1,4 +0,0 @@
-@NotNullByDefault
-package net.hollowcube.mapmaker.multipart;
-
-import org.jetbrains.annotations.NotNullByDefault;
