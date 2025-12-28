7ed58e5e3f19d93d728c5b8d3a7e4db3d343322f
mworzala <mattheworzala@gmail.com>
2025-12-28 09:30:58 -0500

    fix(map-isolate): missing @RuntimeGson on PlayerDataUpdateRequest

--- modules/core/src/main/java/net/hollowcube/mapmaker/player/PlayerDataUpdateRequest.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/player/PlayerDataUpdateRequest.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.annotations.SerializedName;
+import net.hollowcube.common.util.RuntimeGson;
 import net.hollowcube.mapmaker.cosmetic.CosmeticType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -12,6 +13,7 @@ import org.jetbrains.annotations.Nullable;
 import java.time.Instant;
 import java.util.List;
 
+@RuntimeGson
 public class PlayerDataUpdateRequest {
     private String username = null;
     private List<String> ipHistory = null;
