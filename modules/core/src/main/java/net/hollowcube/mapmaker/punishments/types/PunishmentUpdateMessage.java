ecdc43781ad328cf8e250e67f8e54f5e83508dbc
mworzala <mattheworzala@gmail.com>
2026-01-19 09:56:05 -0500

    fix: runtime gson on punishment message

--- modules/core/src/main/java/net/hollowcube/mapmaker/punishments/types/PunishmentUpdateMessage.java
+++ modules/core/src/main/java/net/hollowcube/mapmaker/punishments/types/PunishmentUpdateMessage.java
@@ -1,7 +1,9 @@
 package net.hollowcube.mapmaker.punishments.types;
 
+import net.hollowcube.common.util.RuntimeGson;
 import org.jetbrains.annotations.NotNull;
 
+@RuntimeGson
 public record PunishmentUpdateMessage(@NotNull Action action, @NotNull Punishment punishment) {
 
     // Serialized as ordinal
