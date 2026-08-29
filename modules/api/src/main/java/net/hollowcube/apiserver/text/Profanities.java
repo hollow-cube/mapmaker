444b24b56a23314b89f0514f931e099758d4e45f
mworzala <mattheworzala@gmail.com>
2026-08-29 19:07:06 -0400

    fix: add 88 negatives to Profanities from a sweep of the production censored-message log, mostly non-english words (molesta, cumpl, chlopaki, tidspunkt, weniger, alkakoon) and recurring typos (lkike, ocunting, aboslutely, entitty, recoonect)
    Found by running the 5196 rows of censored.csv back through the filter and having five models propose negatives over what still matched; 144 of the messages are no longer censored and no real hit was let through.

--- modules/api/src/main/java/net/hollowcube/apiserver/text/Profanities.java
+++ modules/api/src/main/java/net/hollowcube/apiserver/text/Profanities.java
@@ -21,15 +21,15 @@ final class Profanities {
         term("arsehol");
         term("arselick");
         term("asslick");
-        term("arsch");
+        term("arsch", "barsch", "marsch");
         term("asshol");
         term("auschwitz");
-        term("beaner");
+        term("beaner", "beanery");
         term("bestiality");
-        term("baise");
+        term("baise", "unbaised");
         term("bakachon");
         term("bakatyon");
-        term("bastard", "bastardized");
+        term("bastard", "bastardiz", "bastardis");
         term("bitch");
         term("btch");
         term("biatch");
@@ -47,20 +47,21 @@ final class Profanities {
         term("cazzo");
         term("ceemen");
         term("chankoro");
-        term("chink", "pachinko");
+        term("chink", "pachinko", "chinkapin", "chinkara", "chinking");
         term("chingchong");
         term("choad");
-        term("chode");
+        term("chode", "psychodelic");
         term("chlamydia");
-        term("clit", "clitheroe");
+        term("clit", "clitheroe", "clitch", "clitic", "clitellum");
         term("clitoris");
         term("cock",
             "cockade", "cockatiel", "cockatiels", "cockatoo", "cockatoos", "cockatrice", "cockayne", "cockburn",
             "cockcroft", "cocked", "cocker", "cockerel", "cockers", "cockeyed", "cockiness", "cocking", "cocklebur",
             "cockney", "cockpit", "cockpits", "cockroach", "cockroaches", "cockscomb", "cockspur", "cocktail", "cockle",
             "cockamamie", "cockamamy", "babcock", "gamecock", "hancock", "haycock", "hitchcock", "leacock", "peacock",
-            "poppycock", "shuttlecock", "stopcock", "woodcock");
-        term("coon", "cocoon", "laocoon", "raccoon", "racoon", "tycoon");
+            "poppycock", "shuttlecock", "stopcock", "woodcock",
+            "cockchafer", "cockcrow", "cocky", "cockapoo", "cocksure", "cockswain");
+        term("coon", "cocoon", "laocoon", "raccoon", "racoon", "tycoon", "coccoon", "recoonect", "coonhound");
         term("cocain");
         term("coitus");
         term("cottonpic");
@@ -72,97 +73,103 @@ final class Profanities {
             "talcum", "taraxacum", "tecumseh", "tucuman", "capsicum", "cecum", "circum", "colchicum", "document",
             "ecumeni", // ecumenical, ecumenism
             "illyricum",
-            "incumben"); // incumbent, incumbency
-        term("cunt", "scunthorpe");
+            "incumben", // incumbent, incumbency
+            "cumminity", "cumpl", "cumin", "cumquat", "cumbia", "cumbre", "cummins", "documet");
+        term("cunt", "scunthorpe", "acocunt", "ocunting");
         term("cvnt");
         term("cunny");
         term("cunnie");
         term("csam");
-        term("cyka");
+        term("cyka", "bouncykath");
         term("darkie");
         term("dick",
             "chappaquiddick", "dickens", "dickensian", "dickerson", "dickey", "dickies", "dickinson", "dickson",
-            "dickvandyke", "dicky", "riddick");
+            "dickvandyke", "dicky", "riddick",
+            "dicker", "dickcissel");
         term("dildo");
         term("douchebag");
         term("dyke", "vandyke");
         term("downie");
         term("dumbass");
         term("ejaculate");
-        term("fag", "antofagasta", "serfage", "wharfage", "fagin", "leafage");
-        term("feck");
+        term("fag", "antofagasta", "serfage", "wharfage", "fagin", "leafage", "cofagrigu", "naturfag");
+        term("feck", "feckless");
         term("fellate");
         term("fellatio");
-        term("felch");
+        term("felch", "apfelchips", "felchen");
         term("fuck");
         term("fvck");
         term("fxck");
-        term("fack");
+        term("fack", "fackel");
         term("fzck");
         term("fck");
         term("fudgepacker");
-        term("flange", "flanged", "flanges");
+        term("flange", "flanged", "flanges", "flanger");
         term("gestapo");
         term("gook");
         term("horny", "thorny");
         term("hooker");
         term("hitler");
         term("incest");
-        term("jap", "japan");
+        term("jap", "japan", "japko", "japonic", "japchae", "jape", "perjaps");
         term("jizz");
         term("jigabo");
         term("junglebunny");
         term("kkk");
-        term("kike");
+        term("kike", "lkike");
         term("klux");
         term("kluklux");
         term("klukluxklan");
-        term("koon");
+        term("koon", "alkakoon", "pakoon", "sekoon", "olkoon", "tulkoon");
         term("lickmy");
         term("masturbat");
-        term("molest");
+        term("molest", "molesta", "moleste", "molesti", "molesto");
         term("muff",
             "muffed", "muffin", "muffins", "muffle", "muffled", "muffler", "mufflers", "muffles", "muffling", "muffs",
             "ragamuffin", "earmuff", "earmuffs");
-        term("nazi", "ashkenazi", "ashkenazic", "ashkenazim", "monazite");
+        term("nazi", "ashkenazi", "ashkenazic", "ashkenazim", "monazite",
+            "amnazing", "naziva", "nazim", "nazir", "nazional");
         term("nigg");
-        term("niqa");
+        term("niqa", "niqab");
         term("nigga");
         term("niqqa");
         term("niggu");
         term("niqqu");
         term("niggr");
-        term("niger"); // yes, this will false flag, but according to Seth it's worth it
+        term("niger", // yes, this will false flag, but according to Seth it's worth it
+            "nigeria", "nigerien", "weniger", "einiger", "sonniger");
         term("nigger");
         term("niglet");
         term("nignog");
-        term("paki", "pakistan");
-        term("penis", "penistone");
+        term("paki", "pakistan", "chlopaki", "paking");
+        term("penis", "penistone", "openish", "happenis", "penisola", "peniscola");
         term("porn");
         term("prostitut");
-        term("pube", "puberty", "pubescent");
+        term("pube", "puber", "pubesc");
         term("pussie");
-        term("pussy", "pussycat", "pussyfoot");
+        term("pussy", "pussycat", "pussyfoot", "pussywillow");
         term("raghead");
         term("rape",
             "grape", "trapeze", "trapezium", "trapezius", "trapezoid", "therapeutic", "drape", "parapet", "rapeseed",
-            "scrape", "serape");
-        term("rapist", "therapist");
+            "scrape", "serape",
+            "rapelle", "rattrape", "trapes", "wraped");
+        term("rapist", "therapist", "tharapist");
         term("retard", "retardant", "retarder", "retarding");
         term("rimjob");
-        term("shit", "cushitic", "shitake", "peshitta", "libshitz", "shitzu", "yamashita");
-        term("slut");
-        term("spunk", "spunky");
+        term("shit", "cushitic", "shitake", "peshitta", "libshitz", "shitzu", "yamashita",
+            "screnshit", "zhanshishiti");
+        term("slut", "aboslutely");
+        term("spunk", "spunky", "tidspunkt");
         term("suckmy");
         term("sodom");
-        term("semen", "sement", "horsemen", "norsemen", "wisemen"); // basement, casement
+        term("semen", "sement", "horsemen", "norsemen", "wisemen", "advertisemen"); // basement, casement
         term("teensex");
         term("tittie");
-        term("titty");
+        term("titty", "entitty");
         term("trannie");
         term("tranny");
         term("vagina");
-        term("wank", "swank", "wankel");
+        term("wank", "swank", "wankel", "obiwankenobi", "thwanky");
         term("wetback");
         term("whore");
         term("whitepower");
